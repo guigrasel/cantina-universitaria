0d7b59351985fa212071342c80e3775fb49aabb0
Guilherme Grasel <guigrasel@gmail.com>
2025-06-17 19:35:07 -0300

    ♻️ Refatora classe Estoque para usar só ArrayList

--- Estoque.java
+++ Estoque.java
@@ -1,36 +1,35 @@
 import java.util.ArrayList;
-import java.util.List;
 
 public class Estoque {
-    private List<Produto> produtos;
+  private ArrayList<Produto> produtos;
 
-    public Estoque() 
-    {
-      produtos = new ArrayList<>();
-    }
+  public Estoque() 
+  {
+    produtos = new ArrayList<>();
+  }
 
-    public void adicionarProduto(Produto produto) 
-    {
-      produtos.add(produto);
-    }
+  public void adicionarProduto(Produto produto) 
+  {
+    produtos.add(produto);
+  }
 
-    public void removerProduto(Produto produto) 
-    {
-      produtos.remove(produto);
-    }
+  public void removerProduto(Produto produto) 
+  {
+    produtos.remove(produto);
+  }
 
-    public Produto buscarProdutoPorId(int id) 
-    {
-      for (Produto p : produtos) {
-        if (p.getId() == id) {
-          return p;
-        }
-      }
-      return null;
+  public Produto buscarProdutoPorId(int id) 
+  {
+    for (Produto p : produtos) {
+    if (p.getId() == id) {
+      return p;
     }
+    }
+    return null;
+  }
 
-    public List<Produto> getProdutos() 
-    {
-      return produtos;
-    }
+  public ArrayList<Produto> getProdutos() 
+  {
+    return produtos;
+  }
 }
