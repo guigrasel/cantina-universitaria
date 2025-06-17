6ad146f37955cd218323145853f8076ed56b43b2
Guilherme Grasel <guigrasel@gmail.com>
2025-06-17 19:29:40 -0300

    ✨ Adiciona classe Usuário

---
+++ Usuario.java
@@ -0,0 +1,27 @@
+public class Usuario {
+    private int id;
+    private String nome;
+    private String tipo; // "ALUNO", "PROFESSOR", "VISITANTE", "ADMIN"
+
+    public Usuario(int id, String nome, String tipo) 
+    {
+      this.id = id;
+      this.nome = nome;
+      this.tipo = tipo;
+    }
+
+    public int getId() 
+    { 
+      return id; 
+    }
+
+    public String getNome() 
+    { 
+      return nome; 
+    }
+
+    public String getTipo() 
+    { 
+      return tipo; 
+    }
+}
