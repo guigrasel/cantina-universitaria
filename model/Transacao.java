8e13e6dade5b7ccffca802f1b0dfbee0275bc525
Guilherme Grasel <guigrasel@gmail.com>
2025-07-01 22:01:04 -0300

    🔥 Remove código desnecessário

--- model/Transacao.java
+++ model/Transacao.java
@@ -4,43 +4,37 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 public class Transacao {
-    private static int proximoId = 1;
+  private static int proximoId = 1;
 
-    private int id;
-    private LocalDateTime dataHora;
-    private ArrayList<Produto> produtos;
-    private double valorTotal;
+  private int id;
+  private LocalDateTime dataHora;
+  private ArrayList<Produto> produtos;
+  private double valorTotal;
 
-    public Transacao(ArrayList<Produto> produtos, double valorTotal) {
-      this.id = proximoId++;
-      this.dataHora = LocalDateTime.now();
-      this.produtos = produtos;
-      this.valorTotal = valorTotal;
-    }
+  public Transacao(ArrayList<Produto> produtos, double valorTotal) {
+    this.id = proximoId++;
+    this.dataHora = LocalDateTime.now();
+    this.produtos = produtos;
+    this.valorTotal = valorTotal;
+  }
 
-    public int getId() 
-    { 
-      return id; 
-    }
+  public int getId() 
+  { 
+    return id; 
+  }
 
-    public LocalDateTime getDataHora() 
-    { 
-      return dataHora; 
-    }
+  public LocalDateTime getDataHora() 
+  { 
+    return dataHora; 
+  }
 
-    public ArrayList<Produto> getProdutos() 
-    { 
-      return produtos; 
-    }
+  public ArrayList<Produto> getProdutos() 
+  { 
+    return produtos; 
+  }
 
-    public double getValorTotal() 
-    { 
-      return valorTotal; 
-    }
-
-    @Override
-    public String toString() 
-    {
-      return "Transação #" + id + " - " + dataHora + " - Total: R$ " + String.format("%.2f", valorTotal);
-    }
+  public double getValorTotal() 
+  { 
+    return valorTotal; 
+  }
 }
