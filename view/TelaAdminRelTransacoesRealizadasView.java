a1793fbb09537d41b44e74f4e996800fb33b8428
Guilherme Grasel <guigrasel@gmail.com>
2025-07-03 18:44:59 -0300

    🔥 Remove coluna inutilizada

--- view/TelaAdminRelTransacoesRealizadasView.java
+++ view/TelaAdminRelTransacoesRealizadasView.java
@@ -20,7 +20,7 @@ public class TelaAdminRelTransacoesRealizadasView extends JFrame {
     setLocationRelativeTo(null);
     setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 
-    String[] colunas = {"ID", "Data/Hora", "Qtd. Produtos", "Total (R$)"};
+    String[] colunas = {"ID", "Data/Hora", "Total (R$)"};
     tableModel = new DefaultTableModel(colunas, 0) 
     {
       @Override
@@ -42,10 +42,10 @@ public class TelaAdminRelTransacoesRealizadasView extends JFrame {
     tableModel.setRowCount(0);
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
     for (Transacao t : historico.getTransacoes()) {
+      System.out.println(t.getProdutos());
       tableModel.addRow(new Object[]{
         t.getId(),
         t.getDataHora().format(formatter),
-        t.getProdutos().size(),
         String.format("%.2f", t.getValorTotal())
       });
     }
