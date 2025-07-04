72495b256f8edb59e0ca3c4cf96d224170882835
Guilherme Grasel <guigrasel@gmail.com>
2025-07-03 21:18:57 -0300

    ✨ Adiciona javadoc da exception

--- exceptions/TrocoInsuficienteException.java
+++ exceptions/TrocoInsuficienteException.java
@@ -1,6 +1,19 @@
 package exceptions;
 
+/**
+ * Exceção lançada quando o caixa não possui troco suficiente para concluir uma operação de pagamento.
+ *
+ * <p>
+ * Esta exceção é utilizada para sinalizar que não há quantidade suficiente
+ * no caixa para fornecer o troco ao cliente.
+ * </p>
+ */
 public class TrocoInsuficienteException extends Exception {
+  /**
+   * Cria uma nova exceção com a mensagem especificada.
+   *
+   * @param mensagem a mensagem explicando o motivo da exceção
+   */
   public TrocoInsuficienteException(String mensagem) {
     super(mensagem);
   }
